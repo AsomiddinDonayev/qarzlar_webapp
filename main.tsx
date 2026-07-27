aba227d4af0072604d1b504a41493fc89284232b
Asomiddin Donayev <asomiddindonayev55@gmail.com>
2026-07-27 08:41:21 +0000

    Yangilanish

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' // Agar CSS faylingiz bo'lsa
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
